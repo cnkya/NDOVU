5f66a19b94d713b08d5ed09c4244a2421e90cf91
christina nkya <mrs.nkya@gmail.com>
2024-11-09 14:12:23 -0500

    push changes

--- property/urls.py
+++ property/urls.py
@@ -9,5 +9,5 @@ path("<int:pk>/", views.PropertyDetailView.as_view(), name="property_detail"),
 path("new/", views.create_property_page, name="property_new"),
 path("save_property/", views.create_property, name="save_property"),
 path("<int:pk>/update_property/", views.PropertyUpdateDetailView.as_view(), name="property_update"),
-path("save_changes/", views.update_property, name="save_changes"),
+path('save_changes/', views.update_property, name='save_changes'),
 ]
